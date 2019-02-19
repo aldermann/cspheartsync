06f13d55f3ba03e2e22dcc9fd766512de3c4b7af
AnTran <vietanisme@gmail.com>
2019-02-19 21:17:05 +0700

    fix #n

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -2,7 +2,7 @@ import os
 
 import pymongo
 
-db_url = os.getenv("DB_URL") or os.getenv("MONGO_URI") or "localhost"
+db_url = os.getenv("DB_URL") or os.getenv("MONGODB_URI") or "localhost"
 db_name = os.getenv("DB_NAME") or "cspheartsync"
 client = pymongo.MongoClient(db_url)
 db = client.get_database(db_name)
