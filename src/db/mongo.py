4ab73b3689d2caaf9c908057e3661ccbe135d200
AnTran <vietanisme@gmail.com>
2019-02-19 20:48:21 +0700

    fix mongodb #3

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -5,4 +5,4 @@ import pymongo
 db_url = os.getenv("DB_URL") or os.getenv("MONGO_URI") or "localhost"
 db_name = os.getenv("DB_NAME") or "cspheartsync"
 client = pymongo.MongoClient(db_url)
-db = client.get_database() or client.get_database(db_name)
+db = client.get_database(db_name)
