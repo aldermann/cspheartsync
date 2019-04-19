5f84a2b128e6a700f48f1c6f01b9b34f463596b1
An <vietanisme@gmail.com>
2019-04-19 16:26:25 +0700

    fix bug #1

--- src/model/User/MongoUser.py
+++ src/model/User/MongoUser.py
@@ -89,7 +89,7 @@ class MongoUser:
         if favourite != "any":
             query["$and"].append({"gender": favourite})
 
-        data = mongo.db.["user"].find(query).sort("enqueue_time",pymongo.ASCENDING).limit(1)[0]
+        data = mongo.db["user"].find(query).sort("enqueue_time",pymongo.ASCENDING).limit(1)[0]
         if data is None:
             return data
         return data["messenger_id"]
